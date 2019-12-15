20a32932293e81f6152a9b15120e95643c1380f4
Wadym121 <wadiming@gmail.com>
2019-12-15 20:58:02 +0000

    інтернет магазин Style

---
+++ js/index.js
@@ -0,0 +1,26 @@
+const clock = document.querySelector('.clock')
+const updateTime = () => clock.innerHTML = new Date().toLocaleTimeString();
+setInterval(updateTime,1000);
+const sect = document.querySelector('section');
+const para = document.createElement('p');
+para.classList.add('dynamic');
+
+document.querySelector('.login-form input[type=submit]')
+    .addEventListener('click', login);
+function login(e) {
+    e.preventDefault();
+    fetch('login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: JSON.stringify({
+            name: document.querySelector('.login-form input[name=name]').value,
+            password: document.querySelector('.login-form input[name=password]').value
+        })
+    })
+    .then(_ => document.querySelector('.login-form').reset());
+}
+$('.show-me').click(() => $('p').toggle(3000));
+
